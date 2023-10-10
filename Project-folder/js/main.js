fc6ef659a0536567154fc5e871100318714f0226
anticlown322 <karasandrey2005@gmail.com>
2023-10-10 23:53:29 +0300

    feat: implement animation to header scrolling

--- Project-folder/js/main.js
+++ Project-folder/js/main.js
@@ -3,10 +3,15 @@
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             headerWrapper.classList.add('header_active');
+            headerWrapper.style.paddingTop = "20px";
+            headerWrapper.style.paddingBottom = "20px";
         }
         else
         {
             headerWrapper.classList.remove('header_active');
+            headerWrapper.style.paddingTop = "86px";
+            headerWrapper.style.paddingBottom = "0";
         }
     }
 }())
+
